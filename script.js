c1c92fd108640e3fe1b789f327c54643b5943089
Pedro Pinho <rihor.pedro@gmail.com>
2019-03-24 21:32:01 -0300

    added style

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const inputDOM = document.querySelector('#name'); // input onde se insere o nome de usuario do github
 const btnSearchDOM = document.querySelector('#search'); // botao para pesquisar usuario
 const listReposDOM = document.querySelector('#list'); // <li> onde ficara os nomes dos repositoris
+const containerInsertDOM = document.querySelector('#container--insert'); // <div> com o <input> e o <button>
 
 // busca pela url e quando acabar a busca ele chama as funções resolve ou reject, dependendo de qual o resultado da busca
 function getRepos(url) {
@@ -31,20 +32,28 @@ function cleanList() {
     }
 }
 
-// coloca uma <li> na lista em quanto estiver fazendo a busca pela url
-function loading() {
-    cleanList();
+function createListItem(text) {
+    let repoName = document.createTextNode(text);
     let listItem = document.createElement('li');
-    let repoName = document.createTextNode("LOADING...");
+    listItem.classList += 'list--item';
     listItem.appendChild(repoName);
-
     listReposDOM.appendChild(listItem);
 }
 
+// coloca uma <li> na lista em quanto estiver fazendo a busca pela url
+function loading() {
+    cleanList();
+    createListItem('LOADING...');
+}
+
 // btnSearch on click, busca o usuario inserido na <input>
+
 btnSearchDOM.onclick = () => {
     searchUserGithub(inputDOM.value);
 }
+
+// searchUserGithub('rihor');
+
 
 // retorna a url formatada 
 function formatarUrlApi(name) {
@@ -54,28 +63,21 @@ function formatarUrlApi(name) {
 // recebe o nome de usuario e chama a função que pesquisa pela url
 function searchUserGithub(name) {
     let url = formatarUrlApi(name);
-
     // função de busca pela url
     getRepos(url).then(answer => {
-
         cleanList();
 
-        // percorre o array de objetos e acessa o nome e então coloca uma <li> para cada item 
-        for (let i = 0; i < answer.length; i++) { // preenche a lista
-            let listItem = document.createElement('li');
-            let repoName = document.createTextNode(answer[i].name);
-            listItem.appendChild(repoName);
 
-            listReposDOM.appendChild(listItem);
+        containerInsertDOM.classList += ' container--insert--height';
+
+        // percorre o array de objetos e acessa o nome e então coloca uma <li> para cada item 
+        for (let i = 0; i < answer.length; i++) { // preenche a lista            
+            createListItem(answer[i].name);
         }
 
     }).catch(errorMessage => {
         // exibe uma mensagem de erro, colocando uma <li> com um aviso
         cleanList();
-        let listItem = document.createElement('li');
-        let repoName = document.createTextNode("ERROR 404");
-        listItem.appendChild(repoName);
-
-        listReposDOM.appendChild(listItem);
+        createListItem('ERROR 404');
     });
 }
